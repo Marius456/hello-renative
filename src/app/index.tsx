ce7dd63e5e2a3e27a73b1c14ea514c50b39b7010
Marius456 <47567608+Marius456@users.noreply.github.com>
2024-11-29 12:23:01 +0200

    add redux text

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import {
-  Text,
-  View,
-} from "react-native";
-import {
-  ThemeProvider,
-} from "../config";
+import { Text, View } from "react-native";
+import { ThemeProvider } from "../config";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../state";
 
+import { Provider as PaperProvider } from "react-native-paper";
+
 const App = () => (
   <ThemeProvider>
+    {/* <PaperProvider> */}
     <ReduxProvider store={store}>
       <View style={{ backgroundColor: "red", height: "500px", width: "500px" }}>
         <Text>TEST</Text>
       </View>
     </ReduxProvider>
+    {/* </PaperProvider> */}
   </ThemeProvider>
 );
 
